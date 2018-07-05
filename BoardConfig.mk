c20d062a1096daa49d389fce0a1c28609d20e9dd
joemossjr16 <joemossjr16@gmail.com>
2018-07-05 12:41:28 -0400

    Update BoardConfig.mk
    fixed vibration thanks to https://gerrit.omnirom.org/#/c/android_bootable_recovery/+/31021/
    fixed security patch problem causing people not to boot when flashed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,10 +93,11 @@ TW_THEME := portrait_hdpi
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
 # MTP seems to cause a kernel panic, at least with some kernels, so disable it for now
-TW_EXCLUDE_MTP := true
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TW_NO_SCREEN_BLANK := true
+# to add vibration repopick 31021
+TW_USE_QCOM_HAPTICS_VIBRATOR := true
 
 # Encryption
-PLATFORM_SECURITY_PATCH := 2025-12-31
+PLATFORM_SECURITY_PATCH := 2018-05-01
 TW_INCLUDE_CRYPTO := true
